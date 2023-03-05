909a2f17cbfeef46bfa8200356eeff64ed6cb50e
ibimina <96787210+ibimina@users.noreply.github.com>
2023-03-05 22:26:59 +0100

    docs: reset template

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,9 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
+import { RouterView } from 'vue-router'
 
 </script>
 
 <template>
-  <header>
-    
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-
-  </header>
-
   <RouterView />
 </template>
 
