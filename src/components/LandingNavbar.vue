4181e5d8de6204444972333f6d8ea919dc17437d
ibimina <96787210+ibimina@users.noreply.github.com>
2023-03-05 22:27:25 +0100

    docs: landing page navbar

---
+++ src/components/LandingNavbar.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import { RouterLink } from 'vue-router'
+</script>
+
+<template>
+    <header>
+        <h1>Buy</h1>
+        <nav>
+            <ul>
+                <li><RouterLink to="/">Home</RouterLink></li>
+                <li><RouterLink to="/product">Product</RouterLink></li>
+                <li><RouterLink to="/about">About</RouterLink></li>
+                <li><RouterLink to="/contact">Contact</RouterLink></li>
+            </ul>
+           <ul>
+                <li><RouterLink to="/login">Login</RouterLink></li>
+                <li><RouterLink to="/register">Register</RouterLink></li>
+           </ul>
+        </nav>
+      <div>
+        <button class="mobile_nav"></button>
+      </div>
+    </header>
+</template>
+
+<style>
+
+</style>
