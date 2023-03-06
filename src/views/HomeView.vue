854f64ac95feed9ee7e6f048c16449fab0f57e5a
ibimina <96787210+ibimina@users.noreply.github.com>
2023-03-06 21:10:17 +0100

    docs: reset asset path

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import LandingNavbar from '@/components/LandingNavbar.vue';
 
+
 </script>
 
 <template>
@@ -14,7 +15,7 @@ import LandingNavbar from '@/components/LandingNavbar.vue';
         <RouterLink to="/products" class="shopping">Shopping now</RouterLink>
       </div>
       <div class="image">
-        <img src="/assets/images/Hero Image.png" alt="a lady with shopping bags">
+        <img src="../assets/images/Hero Image.png" alt="a lady with shopping bags">
       </div>
     </section>
     <section class="brands">
