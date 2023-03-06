604a23fcc5915427f2f06141d9da14ec3fd13e04
ibimina <96787210+ibimina@users.noreply.github.com>
2023-03-07 00:29:22 +0100

    docs: footer component

---
+++ src/components/FooterBar.vue
@@ -0,0 +1,35 @@
+<template>
+    <footer>
+        <ul>
+            <li>Shop by Category
+                <ul>
+                    <li>Skincare</li>
+                    <li>shoes</li>
+                    <li>Electronics</li>
+                    <li>Handbags</li>
+                    <li>Eyewear</li>
+                </ul>
+            </li>
+            <li>About
+                <ul>
+                    <li>Contact Us</li>
+                    <li>About Us</li>
+                </ul>
+            </li>
+            <li>Policy</li>
+        </ul>
+        <section class="contact">
+            <div class="socials">
+                <img src="/assets/images/facebook.png" alt="facebook">
+                <img src="/assets/images/instagram.png" alt="instagram">
+                <img src="/assets/images/twitterpng" alt="twitter">
+            </div>
+            <div class="location">
+                <img src="/assets/images/location.png" alt="location"> <span>Nigeria</span>
+            </div>
+<div class="copyright">
+    © 2021 | ibimina All Rights Reserved
+</div>
+        </section>
+    </footer>
+</template>
