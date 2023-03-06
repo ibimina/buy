102458fe9ce0cbe7d14f43c112631ee20624bbdc
ibimina <96787210+ibimina@users.noreply.github.com>
2023-03-07 00:38:29 +0100

    style: style social icon

--- src/components/FooterBar.vue
+++ src/components/FooterBar.vue
@@ -20,16 +20,28 @@
         </ul>
         <section class="contact">
             <div class="socials">
-                <img src="/assets/images/facebook.png" alt="facebook">
-                <img src="/assets/images/instagram.png" alt="instagram">
-                <img src="/assets/images/twitterpng" alt="twitter">
+                <img src="/images/facebook.png" alt="facebook" class="social">
+                <img src="/images/instagram.png" alt="instagram" class="social">
+                <img src="/images/twitter.png" alt="twitter" class="social">
             </div>
             <div class="location">
-                <img src="/assets/images/location.png" alt="location"> <span>Nigeria</span>
+                <img src="/images/location.png" alt="location"> <span>Nigeria</span>
             </div>
-<div class="copyright">
-    © 2021 | ibimina All Rights Reserved
-</div>
+            <div class="copyright">
+                © 2023 | ibimina All Rights Reserved
+            </div>
         </section>
     </footer>
 </template>
+
+<style scoped>
+.social{
+width: 20px;
+}
+.socials{
+display: flex;
+align-items: center;
+justify-content: center;
+gap: 1em;
+}
+</style>
