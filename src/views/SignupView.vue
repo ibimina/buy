0b46fc276a0c351019dba5b4fa5b1cfcd16d5bcf
ibimina <96787210+ibimina@users.noreply.github.com>
2023-03-07 14:56:08 +0100

    docs: restructure template

--- src/views/SignupView.vue
+++ src/views/SignupView.vue
@@ -1,21 +1,23 @@
 <template>
+    <main>
     <h1>Create an account</h1>
-    <p>Sign up now to get started with an account</p>
-    <form>
-        <label>
-            <span class="label">Username</span>
-            <input type="text" name="username" class="input" required>
-        </label>
-        <label>
-            <span class="label">Email</span>
-            <input type="email" name="email"  class="input" required>
-        </label>
-        <label>
-            <span class="label">Password</span>
-            <input type="password" name="password"  class="input" required>
-        </label>
+        <p>Sign up now to get started with an account</p>
+        <form>
+            <label>
+                <span class="label">Username</span>
+                <input type="text" name="username" class="input" required>
+            </label>
+            <label>
+                <span class="label">Email</span>
+                <input type="email" name="email"  class="input" required>
+            </label>
+            <label>
+                <span class="label">Password</span>
+                <input type="password" name="password"  class="input" required>
+            </label>
 
-        <input type="submit" value="Get started" class="submit">
-    </form>
+            <input type="submit" value="Get started" class="submit">
+        </form>
+    </main> 
 </template>
 <style scoped></style>
