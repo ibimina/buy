b341773f21637ca31226610c37cce39790b858ad
ibimina <96787210+ibimina@users.noreply.github.com>
2023-03-07 23:40:32 +0100

    docs: add user and auth state

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,15 +1,13 @@
-import  { createStore } from 'vuex';
-
+import { createStore } from 'vuex';
+import { createUserWithEmailAndPassword, onAuthStateChanged, updateProfile, type Auth } from "firebase/auth";
+import { auth } from '@/firebase/config';
 
 export const store = createStore({
     state() {
-      return{
-          count: 0
-      }
-    },
-    mutations: {
-        increment(state) {
-            state.count++;
+        return {
+            authIsReady: false,
+            user: null
         }
-    }
+    },
 });
+
