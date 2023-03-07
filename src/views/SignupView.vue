e80f21d1f370d6968eb5d3e44d9397ab349cf808
ibimina <96787210+ibimina@users.noreply.github.com>
2023-03-07 14:54:12 +0100

    docs: add sign up from

---
+++ src/views/SignupView.vue
@@ -0,0 +1,21 @@
+<template>
+    <h1>Create an account</h1>
+    <p>Sign up now to get started with an account</p>
+    <form>
+        <label>
+            <span class="label">Username</span>
+            <input type="text" name="username" class="input" required>
+        </label>
+        <label>
+            <span class="label">Email</span>
+            <input type="email" name="email"  class="input" required>
+        </label>
+        <label>
+            <span class="label">Password</span>
+            <input type="password" name="password"  class="input" required>
+        </label>
+
+        <input type="submit" value="Get started" class="submit">
+    </form>
+</template>
+<style scoped></style>
