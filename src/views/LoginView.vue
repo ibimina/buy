ae13fea8f16c5cff403133ed98d7ea82ef77037f
ibimina <96787210+ibimina@users.noreply.github.com>
2023-03-08 10:17:26 +0100

    docs: login page template

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,3 +1,25 @@
+<script setup lang="ts">
+
+</script>
 <template>
-    <h1>Login</h1>
+    <main>
+        <p>Log in to your Account</p>
+        <p>Welcome back, please enter your details.</p>
+        <form>
+            <label>
+                <span>Email Address</span>
+                <input type="email" required>
+            </label>
+            <label>
+                <span>Password</span>
+                <div class="password_wrapper">
+                    <input type="password" required>
+                    <img src="" alt="open eye" class="eye">
+                </div>
+            </label>
+            <input type="submit" value="Log in">
+        </form>
+        <p>Don't have an account? <RouterLink to="/signup">Sign Up</RouterLink></p>
+
+    </main>
 </template>
