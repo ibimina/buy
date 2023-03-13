2ea8d1f6073eebfe4bb9c5acd543782cef728349
ibimina <96787210+ibimina@users.noreply.github.com>
2023-03-13 10:19:09 +0100

    docs: fetch products hook

---
+++ src/composables/FetchProducts.ts
@@ -0,0 +1,34 @@
+import { ref } from "vue"
+
+const FetchProducts = (url: string) => {
+    type Product ={
+        id: number
+        title: string
+        price: number
+        thumbnail: string
+        description: string
+        rating:number
+        discountPercentage :number
+    }[]
+    const products = ref<Product|null>([])
+    const error = ref(null)
+    const loading = ref(false)
+    const baseUrl = 'https://dummyjson.com'
+    const fetchData = async () => {     
+        try {
+            loading.value = true
+            const res = await fetch(baseUrl + url)
+            if (!res.ok) {
+                throw new Error('Could not fetch products')
+            }
+            const data = await res.json()
+            products.value = data.products
+            loading.value = false
+        } catch (err: any) {
+            error.value = err.message
+            loading.value = false
+        }
+    }
+    return { products, error, loading, fetchData }
+}
+export default FetchProducts
