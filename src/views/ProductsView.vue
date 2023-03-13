e2281a83cdce232bb01e3bc01c39c267cc897432
ibimina <96787210+ibimina@users.noreply.github.com>
2023-03-13 10:54:16 +0100

    docs: add page components

--- src/views/ProductsView.vue
+++ src/views/ProductsView.vue
@@ -1,8 +1,10 @@
 <script setup lang="ts">
 import Navbar from '@/components/NavBar.vue';
 import SideBar from '@/components/SideBar.vue';
+import PromotionBox from '@/components/PromotionBox.vue';
 import { useRouter } from 'vue-router';
 import { useStore } from 'vuex';
+import FooterBar from '@/components/FooterBar.vue';
 
 const router = useRouter()
 const store = useStore()
@@ -12,25 +14,36 @@ const logOut = async (e: Event) => {
     router.push('/')
 }
 
-
 </script>
 <template>
     <Navbar />
-
-    <section class="wrapper">  
+    <section class="wrapper">
         <SideBar />
+
         <div>
-         <h1>Products {{ store?.state?.user?.displayName }}</h1>
-            <button @click="logOut">log out</button>
-        </div>   
+             <PromotionBox url='/products?limit=6' title="Top rating" />
+            <PromotionBox url='/products/category/fragrances?limit=8' title="Best Deals" />
+           
+            <!-- <h1>Products {{ store?.state?.user?.displayName }}</h1>
+                        <button @click="logOut">log out</button> -->
+        </div>
     </section>
+    <FooterBar />
 </template>
 
 <style scoped>
-
-@media (min-width:60rem) { 
-    .wrapper{
-        display: flex;
+.bg {
+    width: 25px;
+    height: 25px;
+    background-repeat: no-repeat;
+    background-position: center;
+    background-size: cover;
+}
+@media (min-width:60rem) {
+    .wrapper {
+      display: grid;
+      grid-template-columns: 1fr 6fr;
+      padding: 1em;
     }
 }
 </style>
