3ed765052c39563fe55816f3a880afbec29788d2
Ibimina hart <ibimina.c.hart@gmail.com>
2023-03-14 18:30:20 +0100

    docs: refactor code

--- src/components/DealProductCard.vue
+++ src/components/DealProductCard.vue
@@ -14,10 +14,7 @@ const props = defineProps<{
   
      <img :src="props.thumbnail" :alt="props.description" class="img">   
         <div class="col_two">
-        <div class="flex">
                 <h3 class="title">{{ props.title }}</h3>
-                <button class="bg like"></button>
-            </div>
             <div class="price_wrapper">
                 <p class="price">${{ props.price }}</p>
             </div>
@@ -25,22 +22,10 @@ const props = defineProps<{
    
 </template>
 <style scoped>
-.bg {
-    width: 25px;
-    height: 25px;
-    background-repeat: no-repeat;
-    background-position: center;
-    background-size: cover;
-    border: none;
-    background-color: transparent;
-}
 .price{
     font-size: 14px;
     font-weight: 700;
     color: #000;
-}
-.like {
-    background-image: url('../assets/categories/icons8-wishlist.png');
 }
 .img{
     border-top-right-radius: 8px;
@@ -48,12 +33,7 @@ const props = defineProps<{
     height: 200px;
 }
 .title{
- 
     font-size: 12px;
-}
-.flex{
-    display: grid;
-    grid-template-columns: 90% 10%;
 }
 .col_two{
     padding: 1em;
