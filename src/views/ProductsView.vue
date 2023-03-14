a979cdb77bf0ae6be46c745e26cc240fb786a142
Ibimina hart <ibimina.c.hart@gmail.com>
2023-03-14 18:29:55 +0100

    docs: refactor code

--- src/views/ProductsView.vue
+++ src/views/ProductsView.vue
@@ -2,43 +2,22 @@
 import Navbar from '@/components/NavBar.vue';
 import SideBar from '@/components/SideBar.vue';
 import PromotionBox from '@/components/PromotionBox.vue';
-import { useRouter } from 'vue-router';
-import { useStore } from 'vuex';
 import FooterBar from '@/components/FooterBar.vue';
-
-const router = useRouter()
-const store = useStore()
-const logOut = async (e: Event) => {
-    e.preventDefault();
-    await store.dispatch("logOut")
-    router.push('/')
-}
 
 </script>
 <template>
     <Navbar />
     <section class="wrapper">
         <SideBar />
-
         <div>
              <PromotionBox url='/products?limit=6' title="Top rating" />
-            <PromotionBox url='/products/category/fragrances?limit=8' title="Best Deals" />
-           
-            <!-- <h1>Products {{ store?.state?.user?.displayName }}</h1>
-                        <button @click="logOut">log out</button> -->
-        </div>
+            <PromotionBox url='/products/category/fragrances?limit=8' title="Best Deals" />        
+            </div>
     </section>
     <FooterBar />
 </template>
 
 <style scoped>
-.bg {
-    width: 25px;
-    height: 25px;
-    background-repeat: no-repeat;
-    background-position: center;
-    background-size: cover;
-}
 @media (min-width:60rem) {
     .wrapper {
       display: grid;
