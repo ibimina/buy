00a27aaf3be1613ce67643c550e8b2e48dac9317
Ibimina hart <ibimina.c.hart@gmail.com>
2023-03-15 16:15:45 +0100

    docs: add nested route view

--- src/views/ProductView.vue
+++ src/views/ProductView.vue
@@ -1,30 +1,54 @@
 <script  lang="ts" setup>
-import { useRoute, useRouter } from 'vue-router';
+import { useRoute } from 'vue-router';
 import { onMounted } from 'vue';
 import FooterBar from '@/components/FooterBar.vue';
 import Navbar from '@/components/NavBar.vue';
 import FetchProduct from '@/composables/FetchProduct';
 import ProductDetails from '@/components/ProductDetails.vue';
+import LoadingImg from '@/components/LoadingImg.vue';
+import PreviousArr from '@/components/PreviousArr.vue';
 
 const route = useRoute()
-const router = useRouter();
 const { id } = route.params
-
 const { loading, error, fetchData, product } = FetchProduct(`/products/${id}`);
 onMounted(() => {
     fetchData();
 });
-const handleback = () => {
-    router.go(-1)
-}
+const nestedRoute = `/product/${id}/discounts`
 </script>
 <template>
-
+    <main class="proddesc_wrapper">
     <Navbar />
-    <button @click="handleback">go back</button>
-    <h1><span>{{ product.category }}</span> > <span>{{ product.brand }}</span></h1>
-     <p v-if="loading">loading</p>
-     <p v-if="error">error</p>
-     <ProductDetails :product="product"/>
+       <PreviousArr/>
+        <div class="flex"><span class="bold">{{ product.category }}</span> 
+            <img src="/images/icon-next.svg" alt="next " class="dir"> 
+            <span class="bold">{{ product.brand }}</span></div>
+        <p v-if="error">error</p> 
+        <p v-if="loading"><LoadingImg/></p>
+         <ProductDetails :product="product" v-else/>
+         <routerLink :to="nestedRoute">You may also like</routerLink>
+         <router-view></router-view>
+    </main>
+    
     <FooterBar/>
 </template>
+<style scoped>
+.dir{
+    width: 2em;
+    height: 2em;
+}
+.bold{
+    font-weight: 600;
+    font-size: 1.9em;
+    text-transform: capitalize;
+}
+.proddesc_wrapper{
+    padding:1em 2em;
+}
+.flex{
+    display: flex;
+    align-items: center;
+    gap: 1em;
+    padding: 0 2em;
+}
+</style>
