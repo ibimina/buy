283fb71b5690ff1b9db081570f88ffab296d56fb
Ibimina hart <ibimina.c.hart@gmail.com>
2023-03-18 14:46:21 +0100

    docs: styled component

--- src/views/ProductsView.vue
+++ src/views/ProductsView.vue
@@ -6,23 +6,32 @@ import FooterBar from '@/components/FooterBar.vue';
 
 </script>
 <template>
-    <Navbar />
-    <section class="wrapper">
-        <SideBar />
-        <div>
-            <PromotionBox url='/products?limit=6' title="Top rating" />
-            <PromotionBox url='/products/category/fragrances?limit=8' title="Best Deals" />        
-            </div>
-    </section>
+    <div class="paddin">
+     <Navbar />
+        <section class="wrapper">
+            <SideBar />
+            <div>
+                <PromotionBox url='/products?limit=6' title="Top rating" />
+                <PromotionBox url='/products/category/fragrances?limit=8' title="Best Deals" />        
+                </div>
+        </section>
+    </div>  
     <FooterBar />
 </template>
 
 <style scoped>
+.paddin {
+    padding: 1.5em 2em;
+}
+.wrapper {
+    display: grid;
+    grid-template-columns: 1fr;
+    margin: 2em auto;
+}
 @media (min-width:60rem) {
     .wrapper {
       display: grid;
       grid-template-columns: 1fr 6fr;
-      padding: 1em;
     }
 }
 </style>
