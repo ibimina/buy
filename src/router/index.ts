2b3675561293fa49fba7a6a6cef6b08a3bfe2d59
Ibimina hart <ibimina.c.hart@gmail.com>
2023-03-18 19:29:36 +0100

    docs: add checkout route

--- src/router/index.ts
+++ src/router/index.ts
@@ -28,6 +28,11 @@ const router = createRouter({
       meta: { auth: true }
     },
     {
+      path: '/checkout',
+      name: 'checkout',
+      component: () => import('../views/CheckoutView.vue')
+    },
+    {
       path: '/contact',
       name: 'contact',
       component: () => import('../views/ContactView.vue')
